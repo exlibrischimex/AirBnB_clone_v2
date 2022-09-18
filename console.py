a2e0c13885f632c2be40f08549becea08a9653c1
exlibrischimex <exlibrischimex@gmail.com>
2022-09-18 05:32:25 +0100

    update

--- console.py
+++ console.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
-import sys
+from shlex import split
+from models import storage
+from datetime import datetime
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
 
 
